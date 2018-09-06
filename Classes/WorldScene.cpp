b67db3704f86481a70366371a0abd4bbb2d9cf32
wotosts <tosts522@gmail.com>
2018-09-06 17:12:38 +0900

    try scrollview

--- Classes/WorldScene.cpp
+++ Classes/WorldScene.cpp
@@ -1,15 +1,7 @@
 #include "WorldScene.h"
 
 USING_NS_CC;
-
-WorldScene::WorldScene()
-{
-}
-
-
-WorldScene::~WorldScene()
-{
-}
+USING_NS_CC_EXT;
 
 Scene* WorldScene::createScene()
 {
@@ -26,7 +18,40 @@ bool WorldScene::init() {
 		return false;
 	}
 
-	//code
+	//디바이스 크기 가져옴
+	//Director 가져오기
+	auto director = Director::getInstance();
+
+	//OpenGLView가져오기
+	auto glview = director->getOpenGLView();
+
+	//OpenGLView에서 DesignResolutionSize 가져옴
+	auto winSize = glview->getDesignResolutionSize();
+
+	auto backLayer = Layer::create();
+	auto worldmap = Sprite::create("worldImg\\worldmap.png");
+
+	backLayer->setPosition(Point(winSize.width / 2, winSize.height / 2));
+	backLayer->setContentSize(winSize);
+
+	backLayer->addChild(worldmap);
+
+	//scrollView = ScrollView::create(winSize, backLayer);
+	//scrollView->retain();
+	//scrollView->setDirection(ScrollView::Direction::BOTH);
+	//scrollView->setContentSize(backLayer->getContentSize());
+	//scrollView->setPosition(Point(winSize.width / 2, winSize.height / 2));
+	//scrollView->setContentOffset(Point::ZERO, false);
+	//scrollView->setContainer(backLayer);
+	//scrollView->setDelegate(this);
+
+	this->addChild(backLayer);
 
 	return true;
 }
+
+
+void WorldScene::scrollViewDidScroll(cocos2d::extension::ScrollView* view)
+{}
+void WorldScene::scrollViewdidZoom(cocos2d::extension::ScrollView* viw)
+{}
--- Classes/WorldScene.h
+++ Classes/WorldScene.h
@@ -1,14 +1,20 @@
 #include "cocos2d.h"
+#include "cocos-ext.h"
 
-class WorldScene : public cocos2d::Layer
+class WorldScene : public cocos2d::Layer, public cocos2d::extension::ScrollViewDelegate
 {
 public : 
-	WorldScene();
-	~WorldScene();
 public:
 	static cocos2d::Scene* createScene();
 	virtual bool init();
 	void menuCloseCallback(cocos2d::Ref* pSender);
 
-	CREATE_FUNC(WorldScene);
+	CREATE_FUNC(WorldScene); 
+
+	cocos2d::extension::ScrollView* scrollView;
+
+	void scrollViewDidScroll(cocos2d::extension::ScrollView* view);
+	void scrollViewdidZoom(cocos2d::extension::ScrollView* viw);
+
+	
 };
